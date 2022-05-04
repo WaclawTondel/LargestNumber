7aca1fdf18412e58680d4207e064bea28df17310
wtondel <wtondel@Sii.pl>
2022-05-05 00:21:07 +0200

    init commit

---
+++ src/main/java/com/sii/largestnumber/LargestnumberApplication.java
@@ -0,0 +1,13 @@
+package com.sii.largestnumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LargestnumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LargestnumberApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sii/largestnumber/LargestnumberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sii.largestnumber;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LargestnumberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
