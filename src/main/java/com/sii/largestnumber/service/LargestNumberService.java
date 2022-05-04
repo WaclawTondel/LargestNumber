7cba9b1ded5b634bbed5b7950e063b0572374032
wtondel <wtondel@Sii.pl>
2022-05-05 00:29:24 +0200

    added acceptance tests

---
+++ src/main/java/com/sii/largestnumber/service/LargestNumberService.java
@@ -0,0 +1,9 @@
+package com.sii.largestnumber.service;
+
+import java.util.List;
+
+class LargestNumberService {
+    String findLargestMumber(List<Integer> numbers) {
+        return null;
+    }
+}
---
+++ src/test/java/com/sii/largestnumber/service/LargestNumberServiceTest.java
@@ -0,0 +1,52 @@
+package com.sii.largestnumber.service;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LargestNumberServiceTest {
+
+    private LargestNumberService testService = new LargestNumberService();
+
+    @Test
+    void shouldFindLargestNumberCase1() {
+        //given
+        List<Integer> numbers = Arrays.asList(50, 2, 1, 9);
+        String expectedNumber = "95021";
+
+        //when
+        String result = testService.findLargestMumber(numbers);
+
+        //then
+        assertEquals(expectedNumber, result);
+    }
+
+    @Test
+    void shouldFindLargestNumberCase2() {
+        //given
+        List<Integer> numbers = Arrays.asList(420, 42, 423);
+        String expectedNumber = "42423420";
+
+        //when
+        String result = testService.findLargestMumber(numbers);
+
+        //then
+        assertEquals(expectedNumber, result);
+    }
+
+    @Test
+    void shouldFindLargestNumberCase3() {
+        //given
+        List<Integer> numbers = Arrays.asList(5, 50, 56);
+        String expectedNumber = "56550";
+
+        //when
+        String result = testService.findLargestMumber(numbers);
+
+        //then
+        assertEquals(expectedNumber, result);
+    }
+}
